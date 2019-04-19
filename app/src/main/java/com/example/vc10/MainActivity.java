f481b387eac7bafc58bce4b1937a0ca75495b4f0
sergioleko <sergioleko93@gmail.com>
2019-04-19 11:33:14 +0300

    19.04.11_18

---
+++ app/src/main/java/com/example/vc10/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.vc10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //switching on fullscreen, setting display always on
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
